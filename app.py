f114c276e81c187db658a5c27b7b5b1f07156ea1
niharikadhy08 <edwards.niharika@gmail.com>
2026-01-01 11:34:02 +0530

    new changes in the msg

--- app.py
+++ app.py
@@ -54,9 +54,10 @@ if len(st.session_state.messages["💬 Chat"]) == 0:
         {
             "role": "assistant",
             "content": (
-                "Hi 👋 I'm PersonaAI!\n\n"
-                "You can chat with me, create content ideas, study concepts or get help with writing.\n\n"
-                "✨ Choose a mode from the sidebar and start typing!"
+                👋 I'm PersonaAI!\n\n"
+                    "You can chat with me, create content ideas, study concepts "
+                    "or get help with writing.\n\n"
+                    "✨ Choose a mode from the sidebar and start typing!
             )
         }
     )
